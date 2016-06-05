02656c025f28736a7e761e66f61e66feecb59540
Erik Vesteraas <erik@vestera.as>
2016-06-05 20:21:35 +0200

    Prototype for stacked history graph added

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ let insert_chart = function(target, data, colors) {
 
   svg.selectAll("rect")
       .data(data)
-      .enter().append("rect")
+    .enter().append("rect")
       .attr("class", (d, i) => "rect-" + i)
       .attr("width", d => scale(d.value))
       .attr("height", height)
@@ -100,4 +100,74 @@ let insert_chart = function(target, data, colors) {
       expanded = false;
     }
   });
+};
+
+let insert_stacked = function(target, data, colors) {
+  const height = 300;
+  const margin = {
+    left: 30,
+    right: 30,
+    top: 0,
+    bottom: 30
+  };
+
+  data.forEach(year => {
+    year.data.forEach(point => {
+      point.year = year.label;
+    });
+  });
+
+  const xScale = d3.scale.linear()
+      .domain(d3.extent(data, d => d.label))
+      .range([0, width]);
+
+  const yScale = d3.scale.linear()
+      .domain([0, 1])
+      .range([0, height]);
+
+  const svg = d3.select(target).append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .attr("style", `margin-left: -${margin.left}px; ` +
+                     `margin-right: -${margin.right}px;`)
+    .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  const xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient("bottom")
+      .ticks(data.length)
+      .tickValues(data.map(x => x.label))
+      .tickFormat(d3.format("d"));
+
+  const stacking = d3.layout.stack()
+      .offset("expand")
+      .values(d => d.values)
+      .x(d => d.year)
+      .y(d => d.value);
+
+  const unnested = [].concat.apply([], data.map(year => year.data));
+
+  const nested = d3.nest()
+      .key(d => d.label)
+      .entries(unnested);
+
+  const layers = stacking(nested);
+
+  const area = d3.svg.area()
+      .x(d => xScale(d.year))
+      .y0(d => yScale(d.y0))
+      .y1(d => yScale(d.y0 + d.y));
+
+  svg.selectAll(".layer")
+      .data(layers)
+    .enter().append("path")
+      .attr("class", "layer")
+      .attr("d", d => area(d.values))
+      .style("fill", (d, i) => colors[i]);
+
+  svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", `translate(0,${height})`)
+      .call(xAxis);
 };
